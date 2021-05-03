3680d2787b7b01198223784ce769b208487cedae
Uni <a5304186@gmail.com>
2021-05-03 20:55:31 +0800

    ch7

--- servers/http.js
+++ servers/http.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var cors = require('cors');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var resources = require('../resources/model');
 var sensorRoutes = require('../routes/sensors');
 var actuatorRoutes = require('../routes/actuators');
-var converter = require('../middleware/converter');
+// var converter = require('../middleware/converter');
 
 var app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(cors()); // CORS support
 
 // bind route to Express program
@@ -20,6 +20,6 @@ app.get('/pi', (req, res) => {
   res.send('This is the WoT-Pi');
 });
 
-app.use(converter());
+// app.use(converter());
 
 module.exports = app;
