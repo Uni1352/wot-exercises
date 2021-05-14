68ffcf35cd649f6a27f2b47b4ac59cf35f598e8a
Uni <a5304186@gmail.com>
2021-05-14 20:56:50 +0800

    fix: representation converter only use default

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -35,7 +35,36 @@ function generateRepresentationForm(req, res, next) {
   }
 }
 
+module.exports = () => {
+  return (req, res, next) => {
+    console.info('Representation Converter Middleware Called!');
 
-module.exports = {
-  representationConverter: () => generateRepresentationForm
+    if (req.result) {
+      switch (req.accepts) {
+        case 'json':
+          console.info('JSON Representation Selected!');
+          res.send(req.result);
+          break;
+        case 'html':
+          const transform = {
+            'tag': 'div',
+            'html': '${name}:${value}'
+          };
+
+          console.info('HTML Representation Selected!');
+          res.send(json2html.transform(req.result, transform));
+          break;
+        case 'application/x-msgpack':
+          console.info('MessagePack Representation Selected!');
+          res.type('application/x-msgpack');
+          res.send(msgpackEncoder(req.result));
+          break;
+        default:
+          console.info('Defaulting to JSON Representation!');
+          res.send(req.result);
+      }
+    } else {
+      next();
+    }
+  }
 };
