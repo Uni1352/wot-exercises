8d2697f04fe8739cc2e5305f0681bfbc063c6152
Uni <a5304186@gmail.com>
2021-05-20 11:07:54 +0800

    [modified] router

--- servers/http.js
+++ servers/http.js
@@ -22,6 +22,10 @@ app.use(cors());
 // app.use('/actions', actionsRoute);
 app.use('/', routeCreator(model));
 
+app.use(function (req, res, next) {
+  res.status(404).send('Unable to find the requested resource!');
+});
+
 // representation converter
 app.use(converter());
 
