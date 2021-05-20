cb10f8b670b7908409a90b0883a1c3ccb844ffc4
Uni <a5304186@gmail.com>
2021-05-20 21:23:36 +0800

    corePlugin.js: proxy

--- plugins/corePlugin.js
+++ plugins/corePlugin.js
@@ -48,12 +48,14 @@ class CorePlugin {
     this.actions.forEach((actionId) => {
       target[actionId].data = new Proxy(target[actionId].data, {
         set: (arr, index, val) => {
-          console.info(`[proxy] plugin action detected: ${actionId}`);
-          console.info(`array: ${arr}`);
-          console.info(`index: ${index}`);
-          arr[index] = val;
-          console.info(target[actionId].data);
-          // this.doActions(val);
+          if (typeof index === 'number') {
+            console.info(`[proxy] plugin action detected: ${actionId}`);
+            console.info(`array: ${typeof arr}`);
+            console.info(`index: ${index}`);
+            arr[index] = val;
+            console.info(target[actionId].data);
+            // this.doActions(val);
+          }
           return true;
         }
       });
