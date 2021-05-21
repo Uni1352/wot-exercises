d31a202ce7bac821de70b0bed71aeff6549fc66c
Uni <a5304186@gmail.com>
2021-05-21 21:37:18 +0800

    websocket

--- servers/websocket.js
+++ servers/websocket.js
@@ -9,41 +9,37 @@ function createSocketServer(server) {
   console.info('WebSocket server started...');
 
   wss.on('connection', (ws, req) => {
-    const pathname = req.url;
-
     try {
-      let resourceData = selectResource(pathname);
+      let parts = (req.url.split('/')).shift();
 
-      createProxy(resourceData);
+      console.info(parts);
+      if (parts[0] && parts[1]) {
+        let resourceData = model.links[parts[0]].resources[parts[1]].data;
+
+        resourceData = new Proxy(resourceData, {
+          set: (arr, index, val) => {
+            console.info(arr, index, val);
+            // ws.send([arr, index, val]);
+            ws.send('msg from ws server!');
+            return true;
+          }
+        });
+      }
     } catch (err) {
       console.info(`Unable to observe ${req.url} resource`);
     }
   });
 }
 
-function createProxy(target) {
-  console.info(target);
-
-  target = new Proxy(target, {
-    set: (arr, index, val) => {
-      console.info(arr, index, val);
-      // ws.send([arr, index, val]);
-      ws.send('msg from ws server!')
-    }
-  });
-
-}
-
 function selectResource(pathname) {
-  let parts = pathname.split('/');
+  return pathname.split('/');
 
-  parts.shift();
 
-  if (parts[0] === 'actions')
-    return model.links.actions.resources[parts[1]].data;
-  else if (parts[0] === 'properties')
-    return model.links.properties.resources[parts[1]].data;
-  else return;
+  // if (parts[0] === 'actions')
+  //   return model.links.actions.resources[parts[1]].data;
+  // else if (parts[0] === 'properties')
+  //   return model.links.properties.resources[parts[1]].data;
+  // else return;
 }
 
 module.exports = createSocketServer;
