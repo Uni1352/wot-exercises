a5a47839b1aac738797866a13801fe8fa10f64b8
Uni <a5304186@gmail.com>
2021-05-21 22:42:28 +0800

    websocket

--- servers/websocket.js
+++ servers/websocket.js
@@ -14,33 +14,20 @@ function createSocketServer(server) {
       let parts = getPathnameParts(req.url);
 
       if (parts[0] && parts[1]) {
-        createProxy(model.links[parts[0]].resources[parts[1]].data);
-        // model.links[parts[0]].resources[parts[1]].data = new Proxy(model.links[parts[0]].resources[parts[1]].data, {
-        //   set: (arr, index, val) => {
-        //     if (!isNaN(parseInt(index))) {
-        //       arr[index] = val;
-        //       ws.send(JSON.stringify(val));
-        //     }
-        //     return true;
-        //   }
-        // });
+        model.links[parts[0]].resources[parts[1]].data = new Proxy(model.links[parts[0]].resources[parts[1]].data, {
+          set: (arr, index, val) => {
+            if (!isNaN(parseInt(index))) {
+              arr[index] = val;
+              ws.send(JSON.stringify(val));
+            }
+            return true;
+          }
+        });
       }
     } catch (err) {
       console.info(`Unable to observe ${req.url} resource`);
     }
   });
-}
-
-function createProxy(target) {
-  target = new Proxy(target, {
-    set: (arr, index, val) => {
-      if (!isNaN(parseInt(index))) {
-        arr[index] = val;
-        ws.send(JSON.stringify(val));
-      }
-      return true;
-    }
-  })
 }
 
 function getPathnameParts(pathname) {
