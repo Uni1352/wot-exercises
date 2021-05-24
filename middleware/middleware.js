d91bee15590199314c16abc5dd701bffb9a71509
Uni <a5304186@gmail.com>
2021-05-24 10:54:27 +0800

    modify console messages

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,21 +2,21 @@ const msgpack = require('msgpack5')();
 const msgpackEncoder = msgpack.encode;
 
 function generateRepresentationForm(req, res, next) {
-  console.info('Representation Converter Middleware Called!');
+  console.info('[Info] Representation Converter Middleware Called!');
 
   if (req.result) {
     switch (req.accepts(['json', 'application/x-msgpack'])) {
       case 'json':
-        console.info('JSON Representation Selected!');
+        console.info('[Info] JSON Representation Selected!');
         res.send(req.result);
         break;
       case 'application/x-msgpack':
-        console.info('MessagePack Representation Selected!');
+        console.info('[Info] MessagePack Representation Selected!');
         res.type('application/x-msgpack');
         res.send(msgpackEncoder(req.result));
         break;
       default:
-        console.info('Defaulting to JSON Representation!');
+        console.info('[Info] Defaulting to JSON Representation!');
         res.send(req.result);
     }
   } else if (res.location) {
