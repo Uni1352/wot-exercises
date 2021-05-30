22b3b4434561028e4f709329480fe2c510185482
Uni <a5304186@gmail.com>
2021-05-30 19:28:14 +0800

    api token

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -46,22 +46,19 @@ function generateRepresentationForm(req, res, next) {
 function apiTokenAuthorization(req, res, next) {
   console.log(`${req.method} ${req.path}`);
 
-  if (req.path.substring(0, 5) === '/css/') next();
-  else {
-    const token = req.body.token || req.get('authorization') || req.query.token;
+  const token = req.body.token || req.get('authorization') || req.query.token;
 
-    console.log(req.params);
-    if (!token) return res.status(401).send({
+  console.log(req.params);
+  if (!token) return res.status(401).send({
+    success: false,
+    message: 'API token missing.'
+  });
+  else {
+    if (token !== keys.apiToken) return res.status(403).send({
       success: false,
-      message: 'API token missing.'
+      message: 'API token invalid.'
     });
-    else {
-      if (token !== keys.apiToken) return res.status(403).send({
-        success: false,
-        message: 'API token invalid.'
-      });
-      else next();
-    }
+    else next();
   }
 }
 
