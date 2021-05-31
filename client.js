f32906d54c3c7eb27f460dc54cf264941adcb232
Uni <a5304186@gmail.com>
2021-05-31 22:02:23 +0800

    MongoDB

--- client.js
+++ client.js
@@ -1,10 +1,13 @@
 const mongoClient = require('mongodb').MongoClient;
-const server = require('./wot-server');
-const crud = require('./db/db');
+const curd = require('./db/db');
 
-server();
 
 mongoClient.connect('mongodb://192.168.0.14:27017/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}, crud.connection);
+}, (err, db) => {
+  if (err) throw err;
+
+  console.info('[Info] Connected to MongoDB!');
+  db.close();
+});
