459963f66e8333d3486bdbec1bc03669adccdcf8
Uni <a5304186@gmail.com>
2021-05-31 20:19:18 +0800

    MongoDB

--- wot.js
+++ wot.js
@@ -4,7 +4,7 @@ const curd = require('./db/db');
 
 server();
 
-mongoClient.connect('mongodb://192.168.0.14:27017/test', {
+mongoClient.connect('mongodb://127.0.0.1:27017/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, curd.connection);
