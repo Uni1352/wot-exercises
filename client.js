9723e25d59b10d22bb8b34a76bb26bf9c33bb963
Uni <a5304186@gmail.com>
2021-05-31 22:28:05 +0800

    MongoDB

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const mongoClient = require('mongodb').MongoClient;
 const curd = require('./db/db');
 
 
-mongoClient.connect('mongodb://localhost:27017/test', {
+mongoClient.connect('mongodb://192.168.0.17:27017/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err, db) => {
