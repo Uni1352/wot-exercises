19a3fece82dbca0b1387c160aed6cb2a1e512047
Uni <a5304186@gmail.com>
2021-05-31 19:06:19 +0800

    MongoDB

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoClient = require('mongodb').MongoClient;
-const curd = require('./curd');
-
-const url = 'mongodb://192.168.0.14:27017/test';
-
-// mongoClient.connect(url, curd.connection);
-mongoClient.connect(url, {
-  useUnifiedTopology: true
-}, curd.connection);
--- wot.js
+++ wot.js
@@ -1,3 +1,10 @@
+const mongoClient = require('mongodb').MongoClient;
 const server = require('./wot-server');
+const curd = require('./database/curd');
 
 server();
+
+mongoClient.connect('mongodb://192.168.0.14:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, curd.connection);
