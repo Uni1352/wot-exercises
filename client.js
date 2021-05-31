c8ce1f9f57d5442adc214408fd25bc243f80cccc
Uni <a5304186@gmail.com>
2021-06-01 07:53:35 +0800

    mongodb

--- client.js
+++ client.js
@@ -1,13 +1,15 @@
-const mongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const curd = require('./db/db');
 
-
-mongoClient.connect('mongodb://raspberrypi.local:27017/test', {
-  useNewUrlParser: true,
+const url = 'mongodb://192.168.0.14:27017';
+const dbName = 'test';
+const client = new MongoClient(url, {
   useUnifiedTopology: true
-}, (err, db) => {
+});
+
+client.connect((err) => {
   if (err) throw err;
 
   console.info('[Info] Connected to MongoDB!');
-  db.close();
+  client.close();
 });
