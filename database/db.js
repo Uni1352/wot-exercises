d158dcbc37af4247ddbff5c91438e26f72725b0a
Uni <a5304186@gmail.com>
2021-05-31 18:46:21 +0800

    MongoDB

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const curd = require('./curd');
 
 const url = 'mongodb://192.168.0.14:27017/test';
 
-mongoClient.connect(url, curd.connection);
-// mongoClient.connect(url, {
-//   useUnifiedTopology: true
-// }, curd.connection);
+// mongoClient.connect(url, curd.connection);
+mongoClient.connect(url, {
+  useUnifiedTopology: true
+}, curd.connection);
