116c4bfeaaa67f30f8ca4412f7ea4f615fb25d0c
Uni <a5304186@gmail.com>
2021-05-31 18:30:05 +0800

    MongoDB

--- wot-server.js
+++ wot-server.js
@@ -41,7 +41,12 @@ function createServer(port, secure) {
 
   mongoClient.connect(url, {
     useUnifiedTopology: true
-  }, curd.connection);
+  }, (err, db) => {
+    if (err) throw err;
+
+    console.info('[Info] Connected to MongoDB!');
+    db.close();
+  });
 
   return server;
 }
