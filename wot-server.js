e8c2b27e50eb99e871523d39d647811b210cbb2e
Uni <a5304186@gmail.com>
2021-06-01 09:50:57 +0800

    mongodb

--- wot-server.js
+++ wot-server.js
@@ -55,6 +55,31 @@ function initPlugins() {
   ledsPlugin.startPlugin();
 }
 
+function connectToDB() {
+  const MongoClient = require('mongodb').MongoClient;
+
+  const url = 'mongodb://192.168.0.14:27017'
+  const dbName = 'test';
+  const config = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  const client = new MongoClient(url, config);
+
+  client.connect((err) => {
+    console.info('[Info] Connected to MongoDB!');
+
+    try {
+      const db = client.db(dbName);
+
+    } catch (err) {
+      console.info(err);
+    }
+
+    client.close();
+  });
+}
+
 process.on('SIGINT', () => {
   pirPlugin.stopPlugin();
   ledsPlugin.stopPlugin();
--- wot.js
+++ wot.js
@@ -1,26 +1,3 @@
 const server = require('./wot-server');
 
 server();
-
-const MongoClient = require('mongodb').MongoClient;
-
-const url = 'mongodb://192.168.0.14:27017'
-const dbName = 'test';
-const config = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-const client = new MongoClient(url, config);
-
-client.connect((err) => {
-  console.info('[Info] Connected to MongoDB!');
-
-  try {
-    const db = client.db(dbName);
-
-  } catch (err) {
-    console.info(err);
-  }
-
-  client.close();
-});
