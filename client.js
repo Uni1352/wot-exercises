9d192002712375f926bbddb732b33b94df4c11fa
Uni <a5304186@gmail.com>
2021-06-01 09:31:20 +0800

    mongodb

--- client.js
+++ client.js
@@ -1,20 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://192.168.0.14:27017/test'
+const url = 'mongodb://192.168.0.14:27017'
+const dbName = 'test';
 const config = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   connectTimeoutMS: 5000,
   socketTimeoutMS: 5000
 }
+const client = new MongoClient(url, config);
 
-MongoClient.connect(url, config, (err, db) => {
+client.connect(url, config, (err) => {
   try {
+    const db = client.db(dbName);
+
     console.info('[Info] Connected to MongoDB!');
+    client.close();
   } catch (err) {
     console.info(err);
-  } finally {
-    db.close();
   }
-
 });
