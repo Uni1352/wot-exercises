005ea0e53a423a7a07dab918e0654317575efc32
Uni <a5304186@gmail.com>
2021-06-01 08:45:26 +0800

    mongodb connection test

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://192.168.0.14:27017';
 const dbName = 'test';
 const client = new MongoClient(url, {
   useUnifiedTopology: true
