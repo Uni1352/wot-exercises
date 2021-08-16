a7f5bf103ff8da43426171370cf5ccd7039225c5
Uni <a5304186@gmail.com>
2021-08-16 21:39:29 +0800

    test db: query property

--- utils/utils.js
+++ utils/utils.js
@@ -43,58 +43,71 @@ function extractFields(fields, object, target) {
 function modelToResource(subModel, withValue) {
   let resources = [];
 
-  Object.keys(subModel).forEach((key) => {
+  for (let key of Object.keys(subModel)) {
     let val = subModel[key];
     let resource = {};
 
-    // if (withValue) {
-    // resource.values = val.data[val.data.length - 1];
+    resource.id = key;
+    resource.name = val['name'];
 
-    switch (key) {
-      case 'pir':
-        client
-          .query({
-            query: gql(`query Query {
+    if (val['description']) resource.description = val['description'];
+    if (withValue) {
+      // resource.values = val.data[val.data.length - 1];
+
+      switch (key) {
+        case 'pir':
+          client
+            .query({
+              query: gql(`query Query {
                 pirValues(num:1){
                   presence
                   timestamp
                 }
               }`)
-          })
-          .then(result => {
-              resource.values = result.data.pirValues;
-              console.info('[MongoDB] Get Data Successfully!');
-            },
-            err => console.info(`[MongoDB] Error ocurred: ${err}`))
-          .finally(() => console.info('[MongoDB] Done'));
-        break;
-      case 'leds':
-        client
-          .query({
-            query: gql(`query Query {
+            })
+            .then(result => {
+                resource.values = result.data.pirValues;
+                console.info('[MongoDB] Get Data Successfully!');
+              },
+              err => console.info(`[MongoDB] Error ocurred: ${err}`))
+            .finally(() => console.info('[MongoDB] Done'));
+          break;
+        case 'leds':
+          client
+            .query({
+              query: gql(`query Query {
                     ledValues(num:1){
                       one
                       two
                       timestamp
                     }
                   }`)
-          })
-          .then(result => {
-              resource.values = result.data.ledValues;
-              console.info('[MongoDB] Get Data Successfully!');
-            },
-            err => console.info(`[MongoDB] Error ocurred: ${err}`))
-          .finally(() => console.info('[MongoDB] Done'));
-        break;
+            })
+            .then(result => {
+                resource.values = result.data.ledValues;
+                console.info('[MongoDB] Get Data Successfully!');
+              },
+              err => console.info(`[MongoDB] Error ocurred: ${err}`))
+            .finally(() => console.info('[MongoDB] Done'));
+          break;
+      }
     }
-    // }
-    resource.id = key;
-    resource.name = val['name'];
-
-    if (val['description']) resource.description = val['description'];
 
     resources.push(resource);
-  });
+  }
+
+  // Object.keys(subModel).forEach((key) => {
+  //   let val = subModel[key];
+  //   let resource = {};
+
+
+  //   resource.id = key;
+  //   resource.name = val['name'];
+
+  //   if (val['description']) resource.description = val['description'];
+
+  //   resources.push(resource);
+  // });
   console.info(resources);
 
   return resources;
