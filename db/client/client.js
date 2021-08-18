a01e9a3fa8d34c6f322abaa2767432a1c5dd51a8
Uni <a5304186@gmail.com>
2021-08-18 12:16:39 +0800

    graphql: subscription

--- db/client/client.js
+++ db/client/client.js
@@ -1,8 +1,25 @@
-const { ApolloClient, InMemoryCache, HttpLink } = require('@apollo/client');
+const { ApolloClient, InMemoryCache, HttpLink, split, gql } = require('@apollo/client');
+const { getMainDefinition } = require('@apollo/client/utilities');
+const { SubscriptionClient } = require('subscriptions-transport-ws');
 const fetch = require('cross-fetch');
+const ws = require('ws');
 
 const uri = 'http://192.168.43.61:4000/graphql';
-const link = new HttpLink({ uri, fetch });
+const httpLink = new HttpLink({ uri, fetch });
+const wsLink = new SubscriptionClient(
+  'ws://192.168.43.61:4000/graphql', { reconnect: true }, ws);
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 const cache = new InMemoryCache({ addTypename: false });
 const client = new ApolloClient({ link, cache });
 
